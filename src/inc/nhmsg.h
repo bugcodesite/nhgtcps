4397d3a3094da0e7a7145eaa39b6f2e1632fc9ed
bugecode <373335273@qq.com>
2022-12-07 05:44:23 +0000

    ok

--- src/inc/nhmsg.h
+++ src/inc/nhmsg.h
@@ -1,2 +1,10 @@
-
+typedef struct __ngmsg_s_head{
+    __int8_t sid;//0~7F
+    __int8_t c;
+    __int16_t l;
+} NGMSG_HEAD,*PNGMSG_HEAD;
+typedef struct __ngmsg_s{
+    NGMSG_HEAD head;
+    char *payload;
+} NGMSG,*PNGMSG;
 extern int test();
--- src/main/nhgtcps.c
+++ src/main/nhgtcps.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "../inc/config.h"
 int main(int argc,char**argv){
-    printf("%s",ver);
+    printf("%s\n",ver);
     test();
 }
