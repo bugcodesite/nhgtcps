9edfe28ce088bfe68c90585bd20e349a68d720b8
bugecode <373335273@qq.com>
2023-03-06 02:39:12 +0000

    ok

--- src/inc/lmm.h
+++ src/inc/lmm.h
@@ -4,3 +4,23 @@
 int lmm_end();
 void * lmm_alloc();
 int lmm_free(void *p);
+
+
+typedef struct __lmm_store{
+
+
+} LMMS,PLMMS;
+/**
+    alloc memory block
+*/
+extern void * lmm_get(int size);
+/*
+    free memory
+    if(p==NULL){
+        //really free
+    }else{
+        //mark p
+        //can reuse p
+    }
+*/
+extern void * lmm_free(void *p);
